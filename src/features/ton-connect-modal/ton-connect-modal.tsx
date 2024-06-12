ebf02e8e311541a8702d980728fad4fd87fe957f
0xmusashi <musashihp42@gmail.com>
2024-06-12 17:27:19 +0700

    try to redirect to /whiskers after disconnect

--- src/features/ton-connect-modal/ton-connect-modal.tsx
+++ src/features/ton-connect-modal/ton-connect-modal.tsx
@@ -27,7 +27,7 @@ export const TonConnectModal = () => {
             }
         }
 
-        window.location.href = '/';
+        window.location.href = '/whiskers';
     };
 
     useEffect(() => {
