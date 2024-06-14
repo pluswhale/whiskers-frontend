88b471cfd297f86171405bd15b666ddca99c9c31
0xmusashi <musashihp42@gmail.com>
2024-06-14 12:49:26 +0700

    switch to mainnet config

--- src/contracts/config.ts
+++ src/contracts/config.ts
@@ -1,16 +1,16 @@
-export const CONTRACT_ADDRESS = 'EQC3FRoofH_NRIn_M-VyvqW7Dtodh2EAN378Iz8l9uDR9d8v';
-export const JETTON_MINTER_ADDRESS = 'EQAO_g3zYDPJd8vwxzSPjspogpnEbI1oF4BWtcwvWnaLh3Sc';
-export const NETWORK = 'testnet';
-export const TREASURY_ADDRESS = 'EQAYb6q5B0cA5rYuOw6czluN_McV9Q9k_nTtS97kfEEBR1NN';
-export const TRACE_API = NETWORK == 'testnet'
-    ? 'https://testnet.tonapi.io/v2/traces/'
-    : 'https://tonapi.io/v2/traces/';
-export const CONTRACT_TRACK_API = NETWORK == 'testnet'
-    ? `https://testnet.tonapi.io/v2/blockchain/accounts/${CONTRACT_ADDRESS}/transactions?limit=1000`
-    : `https://tonapi.io/v2/blockchain/accounts/${CONTRACT_ADDRESS}/transactions?limit=1000`;
-export const TREASURY_TRACK_API = NETWORK == 'testnet'
-    ? `https://testnet.tonapi.io/v2/blockchain/accounts/${TREASURY_ADDRESS}/transactions?limit=1000`
-    : `https://tonapi.io/v2/blockchain/accounts/${TREASURY_ADDRESS}/transactions?limit=1000`;
+export const CONTRACT_ADDRESS = 'EQDEgOGuXZE2XfHeQRPILtNDy6twOO3cYxBh9t3Qq6_0B3-F';
+export const JETTON_MINTER_ADDRESS = 'EQCObh4-ZaghOva8uCz_AMMnvifM3PS-EppUJEJQXXnlP0zX';
+export const NETWORK = 'mainnet';
+export const TREASURY_ADDRESS = 'UQCLWHdSKY2XKj9Uq3k3cBFLSkvAeFkq2fuvbu70ZI_Klk6r';
+export const TRACE_API = NETWORK == 'mainnet'
+    ? 'https://tonapi.io/v2/traces/'
+    : 'https://testnet.tonapi.io/v2/traces/';
+export const CONTRACT_TRACK_API = NETWORK == 'mainnet'
+    ? `https://tonapi.io/v2/blockchain/accounts/${CONTRACT_ADDRESS}/transactions?limit=1000`
+    : `https://testnet.tonapi.io/v2/blockchain/accounts/${CONTRACT_ADDRESS}/transactions?limit=1000`;
+export const TREASURY_TRACK_API = NETWORK == 'mainnet'
+    ? `https://tonapi.io/v2/blockchain/accounts/${TREASURY_ADDRESS}/transactions?limit=1000`
+    : `https://testnet.tonapi.io/v2/blockchain/accounts/${TREASURY_ADDRESS}/transactions?limit=1000`;
 export const SPIN_100_PRICE = 3600;
 export const SPIN_250_PRICE = 8900;
 export const SPIN_500_PRICE = 17700;
