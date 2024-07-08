d10d3b7f61c453984834e15f0bd7f76d04ef242e
Ryu <rastsislauyushkevich@gmail.com>
2024-07-08 16:13:32 +0300

    setup tests

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173/whiskers-frontend',
+    viewportHeight: 896,
+    viewportWidth: 414,
+    userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+    defaultCommandTimeout: 7000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/tasks.cy.ts
@@ -0,0 +1,15 @@
+describe('Tasks spec', () => {
+  it('Visits tasks page', () => {
+    cy.visit('/');
+
+    cy.contains('Tasks').click()
+
+    cy.url().should('include', '/tasks')
+  })
+
+  it('Renders invitation tasks', () => {
+    cy.visit('/tasks');
+
+    cy.contains('Invite').should('be.visible');
+  })
+})
