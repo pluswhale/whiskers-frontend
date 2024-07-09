4453df74af8d7b8d3ac4ebfc6620854b5db15470
Egor Dultsev <listenmy@yandex.ru>
2024-07-09 14:36:20 +0300

    Updates

---
+++ assets/Icon-CiTlL9kc.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Blia32k8.js";const s=({style:i,src:o})=>t.jsx("div",{style:i,children:t.jsx("img",{style:{height:"100%",width:"100%",objectFit:"contain"},src:o,alt:"icon"})});export{s as I};
