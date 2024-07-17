b5abc5ccb523a407c280d9ba6a7f77d30541467f
Ryu <rastsislauyushkevich@gmail.com>
2024-07-17 12:03:03 +0300

    tests for wheel

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -2,9 +2,9 @@ describe('Tasks spec', () => {
   it('Visits tasks page', () => {
     cy.visit('/');
 
-    cy.contains('Tasks').click()
+    cy.contains('Tasks').click();
 
-    cy.url().should('include', '/tasks')
+    cy.url().should('include', '/tasks');
   })
 
   it('Renders invitation tasks', () => {
