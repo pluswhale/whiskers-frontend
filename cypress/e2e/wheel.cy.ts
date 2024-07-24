bf99c64dd3ac653734226c37e7373682d09c6652
Ryu <rastsislauyushkevich@gmail.com>
2024-07-24 14:49:35 +0300

    updated tests

--- cypress/e2e/wheel.cy.ts
+++ cypress/e2e/wheel.cy.ts
@@ -23,14 +23,33 @@ describe('Wheel spec', () => {
     cy.get('canvas');
   })
 
-  it('Spin button gets disabled when there are no free spins', () => {
+  it('Spin button gets disabled when there are no spins', () => {
     cy.visit('/');
     cy.get('*[class*="_app__extra_spins__free_spin__score_p"]')
       .invoke('text')
-      .then(spinsValue => {
-        if(Number(spinsValue) == 0) {
-          cy.get('*[class*="_app__spin_button"').first().should('have.property', '*[class*="_disable"]')
-        }
+      .then(freeSpins => {
+        cy.get('*[class*="_app__extra_spins__bonus_spin__buy_spins"]').children().first()
+          .invoke('text')
+          .then(bonusSpins => {
+            if(Number(freeSpins) === 0 && Number(bonusSpins) === 0) {
+              cy.get('*[class*="_app__spin_button"').first().should('have.property', '*[class*="_disable"]')
+            }
+          })
+      })
+  })
+
+  it('Spin button gets enabled when there are spins', () => {
+    cy.visit('/');
+    cy.get('*[class*="_app__extra_spins__free_spin__score_p"]')
+      .invoke('text')
+      .then(freeSpins => {
+        cy.get('*[class*="_app__extra_spins__bonus_spin__buy_spins"]').children().first()
+          .invoke('text')
+          .then(bonusSpins => {
+            if(Number(freeSpins) !== 0 && Number(bonusSpins) !== 0) {
+              cy.get('*[class*="_app__spin_button"').first().should('not.have.property', '*[class*="_disable"]')
+            }
+          })
       })
   })
 })
